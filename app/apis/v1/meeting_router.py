108db8a459c78029f5d75ab15f8a62b88e9da370
grrr127 <qrrr127@gmail.com>
2025-08-20 13:13:46 +0900

    add meeting create spec

---
+++ app/apis/v1/meeting_router.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+
+from app.dtos.create_meeting_response import CreateMeetingResponse
+
+edgedb_router = APIRouter(prefix="/v1/edgedb/meetings", tags=["Meeting"])
+mysql_router = APIRouter(prefix="/v1/mysql/meetings", tags=["Meeting"])
+# 원래는 어떤 DB를 쓰는지 URL에 적을 필요 없음! 강의에서만!
+# 실전에서는 DB이름을 URL에 넣지 마세요!!
+
+
+@edgedb_router.post("", description="meeting을 생성합니다.")
+async def api_create_meeting_edgedb() -> CreateMeetingResponse:
+    return CreateMeetingResponse(url_code="abc")
+
+
+@mysql_router.post("", description="meeting을 생성합니다.")
+async def api_create_meeting_mysql() -> CreateMeetingResponse:
+    return CreateMeetingResponse(url_code="abc")
